b35c52b49443a40de787e2009aa038c8349d7e72
Asaf Varon <asafv@tikalk.com>
2022-11-03 19:15:19 +0200

    add error widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,20 @@ Future<void> main() async {
   Fimber.plantTree(DebugTree());
   WidgetsFlutterBinding.ensureInitialized();
 
+  ErrorWidget.builder = (details) => Material(
+        color: Colors.red,
+        child: Center(
+          child: Text(
+            details.exception.toString(),
+            style: const TextStyle(
+              color: Colors.yellow,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+        ),
+      );
+
   await setupDependencies();
   runApp(const MyApp());
 }
